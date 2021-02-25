72449cf8de6b969cbbcafe35a97e9cc9285b5de5
Sean Dickinson <sedickinson9123@gmail.com>
2021-02-25 08:57:20 -0500

    feat: added initial functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="m-2 p-4">Understanding Mandelbrot</h1>
+    <section class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <h3>C = {{complexExpression}}</h3>
+          <b-form inline>
+            <b-form-group
+            class="p-2"
+            id="c-real"
+            label="Real"
+            label-for="real-input">
+              <b-form-input class="ml-2" type="number" v-model="real" placeholder="Enter the real component" />
+
+            </b-form-group>
+            <b-form-group
+            class="p-2"
+            id="c-imag"
+            label="Imaginary"
+            label-for="imag-input">
+              <b-form-input class="ml-2" type="number" v-model="imag" placeholder="Enter the imaginary component" />
+            </b-form-group>
+             <b-form-group
+            class="p-2"
+            id="iterations"
+            label="# of iterations"
+            label-for="iterations-input">
+              <b-form-input class="ml-2" type="number" v-model="numIterations" />
+            </b-form-group>
+
+          </b-form>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-6">
+          <display-table :values="values"></display-table>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {complex} from 'mathjs';
+import DisplayTable from './components/DisplayTable';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DisplayTable
+  },
+  data(){
+    return {
+      real: 0,
+      imag: 1,
+      numIterations: 20
+    }
+  },
+  computed: {
+    values(){
+      let seed = complex(0,0);
+       const c = complex(this.real, this.imag);
+       const values = [];
+       for(let i = 0; i < this.numIterations; i++){
+         const val = seed.pow(2).add(c);
+         values.push(val.round(5).toString());
+         if(val.toString() === 'Infinity'){
+           return values
+         }
+         seed = val.clone();
+       }
+       return values
+    },
+    complexExpression(){
+      return complex(this.real, this.imag).round(5).toString()
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/DisplayTable.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="display-table py-3">
+  <b-table striped outlined :items="dataItems"></b-table>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayTable',
+  props: {
+    values: Array
+  },
+  computed: {
+    dataItems(){
+      return this.values.map((val, i) => ({
+        iteration: i + 1,
+        value: val
+      }))
+    }
+  }
+}
+</script>
+
