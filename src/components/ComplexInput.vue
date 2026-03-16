f9491c473153a18df0d481a7a633edec3cb5c460
Sean Dickinson <90267290+sean-dickinson@users.noreply.github.com>
2026-03-16 10:14:52 -0400

    fix: correct input parsing (#5)

--- src/components/ComplexInput.vue
+++ src/components/ComplexInput.vue
@@ -3,18 +3,43 @@ import { complex, type Complex } from "mathjs";
 const model = defineModel<Complex>({
   required: true,
   default: () => complex(0, 0),
-  set: (value: Complex) => complex(value.re, value.im),
 });
+
+const coalesceToZero = (value: unknown) => {
+  return Number(value || 0);
+};
+
+const handleRealInput = (event: Event) => {
+  const value = (event.target as HTMLInputElement).value;
+  model.value.re = coalesceToZero(value);
+};
+
+const handleImaginaryInput = (event: Event) => {
+  const value = (event.target as HTMLInputElement).value;
+  model.value.im = coalesceToZero(value);
+};
 </script>
 
 <template>
   <label for="real"
     >Real Component
-    <input id="real" type="number" v-model.number="model.re" class="form-control" />
+    <input
+      id="real"
+      type="number"
+      :value="model.re"
+      @input="handleRealInput"
+      class="form-control"
+    />
   </label>
 
   <label for="imaginary"
     >Imaginary Component
-    <input id="imaginary" type="number" v-model.number="model.im" class="form-control" />
+    <input
+      id="imaginary"
+      type="number"
+      :value="model.im"
+      @input="handleImaginaryInput"
+      class="form-control"
+    />
   </label>
 </template>
